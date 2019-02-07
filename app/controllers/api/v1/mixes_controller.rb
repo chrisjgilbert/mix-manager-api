1e02fe5fa9c8937e0d854082f032e7a24a6ab481
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 16:50:58 +0000

    striped back first mixes controller test so we just look for index method

---
+++ app/controllers/api/v1/mixes_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::MixesController < ApplicationController
+
+end
--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -3,11 +3,8 @@ require 'rails_helper'
 RSpec.describe Api::V1::MixesController, type: :controller do
 
   describe 'GET /index' do
-    let!(:mixes) { create_list(:mix, 10) }
-
     it 'retuns all articles' do
       get :index
-      expect(json.size).to eq(10)
     end
   end
 
