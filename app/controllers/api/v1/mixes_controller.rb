39b9a613e6491fda5a50e44d88324e56db27d8f2
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 17:00:30 +0000

    get mixes returnign all articles as json

--- app/controllers/api/v1/mixes_controller.rb
+++ app/controllers/api/v1/mixes_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::MixesController < ApplicationController
 
+  def index
+    render json: Mix.all
+  end
+
 end
--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::MixesController, type: :controller do
-
-  describe 'GET /index' do
+RSpec.describe Api::V1::MixesController, type: :request do
+  describe 'GET /api/v1/mixes' do
+    let!(:mixes) { create_list(:mix, 10) }
     it 'retuns all articles' do
-      get :index
+      get '/api/v1/mixes'
+      json = JSON.parse(response.body)
+      expect(json.size).to eq(10)
     end
   end
-
 end
