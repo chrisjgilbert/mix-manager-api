ad362b71a7d842502e5c72cf8ceb911a4d1b146b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 17:09:23 +0000

    GET /mixes/:id returns the given mix

--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MixesController, type: :request do
-  describe 'GET /api/v1/mixes' do
-    let!(:mixes) { create_list(:mix, 10) }
-    before { get '/api/v1/mixes' }
+  let!(:mixes) { create_list(:mix, 10) }
 
+  describe 'GET /api/v1/mixes' do
+    before { get '/api/v1/mixes' }
     it 'retuns all articles' do
       json = JSON.parse(response.body)
       expect(json.size).to eq(10)
@@ -12,6 +12,15 @@ RSpec.describe Api::V1::MixesController, type: :request do
 
     it 'returns a 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /mixes/:id' do
+    it 'returns the given the mix' do
+      mix_id = mixes.first.id
+      get "/mixes/#{mix_id}"
+      json = JSON.parse(response.body)
+      expect(json['id']).to eq(mix_id)
     end
   end
 end
