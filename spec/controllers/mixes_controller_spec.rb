bf00d396a73187da2dc2e9be3038ec9612248d15
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 17:02:00 +0000

    mixes controller index returns a 200

--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::MixesController, type: :request do
   describe 'GET /api/v1/mixes' do
     let!(:mixes) { create_list(:mix, 10) }
+    before { get '/api/v1/mixes' }
+
     it 'retuns all articles' do
-      get '/api/v1/mixes'
       json = JSON.parse(response.body)
       expect(json.size).to eq(10)
+    end
+
+    it 'returns a 200' do
+      expect(response).to have_http_status(200)
     end
   end
 end
