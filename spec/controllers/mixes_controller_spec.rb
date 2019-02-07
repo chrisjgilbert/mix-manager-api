ec55d157ca34ceccd08fe944e2a040e0d4924c6f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 16:43:33 +0000

    initial mixes controller index test written. on red

--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe MixesController, type: :controller do
+RSpec.describe Api::V1::MixesController, type: :controller do
+
+  describe 'GET /index' do
+    let!(:mixes) { create_list(:mix, 10) }
+
+    it 'retuns all articles' do
+      get :index
+      expect(json.size).to eq(10)
+    end
+  end
 
 end
