c5dc03899d1ad023b8f972d1fa6eab5dc0404d0f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-07 17:15:27 +0000

    GET /mixes/:id returns a 200 on green

--- spec/controllers/mixes_controller_spec.rb
+++ spec/controllers/mixes_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MixesController, type: :request do
+
   let!(:mixes) { create_list(:mix, 10) }
+  let(:mix_id) { mixes.first.id }
 
   describe 'GET /api/v1/mixes' do
     before { get '/api/v1/mixes' }
@@ -16,11 +18,14 @@ RSpec.describe Api::V1::MixesController, type: :request do
   end
 
   describe 'GET /mixes/:id' do
+    before { get "/api/v1/mixes/#{mix_id}" }
     it 'returns the given the mix' do
-      mix_id = mixes.first.id
-      get "/api/v1/mixes/#{mix_id}"
       json = JSON.parse(response.body)
       expect(json['id']).to eq(mix_id)
+    end
+
+    it 'returns a 200' do
+      expect(response).to have_http_status(200)
     end
   end
 end
