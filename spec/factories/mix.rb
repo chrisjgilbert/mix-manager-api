fb39ac0e778d6ca069401e9cbe390be57626d77f
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-08 15:39:36 +0000

    factorybot tag factory setup

--- spec/factories/mix.rb
+++ spec/factories/mix.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
+  factory :tag do
+    name { Faker::Lorem.word }
+  end
+
   factory :mix do
     title { Faker::Lorem.word }
     url { Faker::Internet.url }
+
+    after(:create) do |mix|
+      mix.tags << FactoryBot.create(:tag)
+    end
   end
 end
--- spec/models/mix_spec.rb
+++ spec/models/mix_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Mix, type: :model do
   it { should validate_presence_of(:url) }
   it { should have_many(:taggings).dependent(:destroy) }
   it { should have_many(:tags).through(:taggings) }
+
+  describe '#tagged_with' do
+    let!(:mix) { FactoryBot.create(:mix) }
+
+    it 'returns the tags on a mix' do
+      expect(Mix.tagged_with(mix.tags.size)).to eq(1)
+    end
+  end
 end
