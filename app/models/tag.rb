90f8bafb8ee69c62a7ce99f6a4c042813c0f7f28
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-08 14:34:07 +0000

    Tag has many taggings and now dependent on

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates_presence_of :name
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :mixes, through: :taggings
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   it { should validate_presence_of(:name) }
-  it { should have_many(:taggings) }
+  it { should have_many(:taggings).dependent(:destroy) }
   it { should have_many(:mixes).through(:taggings) }
 end
