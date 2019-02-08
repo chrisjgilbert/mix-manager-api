625e88192d49f7d3e77fcd13ccb63f69665cda39
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-08 14:35:08 +0000

    mix has_many taggings relationship now dependent: :destroy

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -1,5 +1,5 @@
 class Mix < ApplicationRecord
   validates_presence_of :title, :url
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 end
--- spec/models/mix_spec.rb
+++ spec/models/mix_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Mix, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:url) }
-  it { should have_many(:taggings) }
+  it { should have_many(:taggings).dependent(:destroy) }
   it { should have_many(:tags).through(:taggings) }
 end
